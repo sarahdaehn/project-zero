b1511119aeb0913e1b8d231722babf020ad7bcb2
MacBook <macbook@MacBooks-MacBook-Pro.local>
2022-02-09 21:37:31 +0100

    Add the ability to name my Angry Bird

--- script/app.js
+++ script/app.js
@@ -1,2 +1,130 @@
-console.log("hello project-zero");
+console.log("Angry Bird");
+
+// start the game using a prompt and then a function which starts the game 
+
+
+const nameOfAngrybird  = prompt ("Please enter your Angry Bird's name");
+if (nameOfAngrybird != null){
+    document.getElementById("AngrysBirdsName").innerHTML = 
+    "Let's play with our Angry Bird " +  nameOfAngrybird + "!";
+
+
+    //(`Let's play with your Angry Bird ${nameOfAngrybird}`);
+};
+
+/*<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+Enter your Angry Bird's Name
+</button>
+*/
+
+
+// Display the follwing metrics for your pet 
+
+/*
+Hunger (1-10scale)
+Sleepiness (1-10 scale )
+Boredom(1-10)
+
+*/
+
+
+
+
+
+
+
+
+
+
+//$("#exampleMaodel").modal()
+
+
+
+
+
+
+//let hunger = 0;
+//let sleep = 0;
+//let bored = 0;
+
+/*<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+    Enter your Angry Bird's Name
+  </button> */
+
+//  Ask for the name to start the game 
+//$('.btn btn-primary').on('click', () => {
+	//console.log('It worked!!!!');
+//});
+
+//$('.btn.danger').button('toggle').addClass('fat')
+
+//$(document).on('.btn btn-primary');
+
+//Button function Logic
+
+// feed Pet 
+// When the user clicks on the button hunger he feeds the pet 1 
+
+ //class Angrybird {
+     //constructor (name){
+        //this.name = name;
+        //this.hunger = 0;
+        //this.sleep = 0;
+        //this.bored = 0; 
+    
+    //feed(food){
+     //return this.hunger += food;
+
+
+     //const myAngrybird = new Angrybird (Sarah);
+
+
+     /*class Angrybird {
+         constructor (name){
+             this.name = name;
+             this.hunger = 1;
+             this.sleepiness = 1;
+             this.boredom = 1; 
+             this.age = 1;
+         }
+     }
+
+
+// Increase your pet's Hunger / inside Methods 
+
+food(){
+    if(this.hunger
+}
+
+// Increase your pet's Sleepiness 
+
+
+// Increase your pet's Boredom 
+
+
+
+
+ // Increase your pet's age every x minutes 
+
+ // Increase your pet's Hunger /sleepiness and bores metrics on an interval of your pet 
+
+// your pet should die if hunger sleepiness hits 10
+
+// if this.hunger >= 10; 
+  //  return (maybe a pop up / AngryBird is dead)
+
+
+
+
+
+ hunger (){
+
+ }    
+     
+
+const newAngryBird = new Angrybird("");
+
+console.log(newAngryBird);
+*/
 
